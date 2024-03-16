cdcdcc4da5d850b8cdc90b7303c100e8afd19506
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-16 22:02:40 +0700

    query task

---
+++ api/v1/controllers/task.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import Task from '../models/task.model';
+
+export const index = async (req: Request, res: Response): Promise<void> => {
+    const find = {
+        deleted: false,
+    }
+
+    if(req.query.status){
+        find['status'] = req.query.status;
+    }
+
+    //Sort
+    const sort = {};
+    if(req.query.sortKey && req.query.sortValue){
+        sort[`${req.query.sortKey}`] = req.query.sortValue;
+    }
+    //End Sort
+
+    const tasks = await Task.find(find).sort(sort);
+
+    res.json(tasks);    
+}
+
+// [GET] /api/v1/tasks/detail/:id
+export const detail = async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+  
+    const task = await Task.findOne({
+      _id: id,
+      deleted: false
+    });
+  
+    res.json(task);
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log('OK');
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { connect as connectDatabase } from "./config/database";
+
+import mainV1Routes from "./api/v1/routes/index.route";
+
+dotenv.config();
+connectDatabase();
+
+const app: Express = express();
+const port: string | number = process.env.PORT || 3000;
+
+// API Routes
+mainV1Routes(app);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
